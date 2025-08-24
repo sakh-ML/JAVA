c1aae08c03fd5479c8fc0681e9428f9cb8b5492b
sakh-ML <ml.abusaleh.ml@gmail.com>
2025-08-24 17:35:27 +0200

    new functions where added

--- FileHandling.java
+++ FileHandling.java
@@ -1,13 +1,26 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 //file handling project
 
 public class FileHandling{
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		System.out.println("Ey");
-		createFile("HEy.txt");
+		//createFile("test.txt");
+		//FileOpeartions.createFile("test.txt");
+		//String fileName = "test.txt";
+		//List<Character> fileContent = FileOpeartions.readFileCharForChar(fileName);
+		//FileOpeartions.printArrayOfChars(fileContent);
+		//FileOpeartions.writeToFile(fileName, "yuyu");
+		//fileContent = FileOpeartions.readFileCharForChar(fileName);
+		//FileOpeartions.printArrayOfChars(fileContent);
+		FileOpeartions.deleteFile("test.txt");
+
 	}
+}
+
+class FileOpeartions{
 
 	public static void createFile(String fileName){
 		try{
@@ -27,5 +40,58 @@ public class FileHandling{
 			System.out.println("File cannot be created");
 			e.printStackTrace();
 		}
+	}
+
+	public static List<Character> readFileCharForChar(String fileName) throws IOException{
+
+		List<Character> result = new ArrayList<>();
+
+		File file = new File(fileName);
+		if(!file.exists()){
+
+			throw new FileNotFoundException("File wasnt found !");
+		}
+		else{
+			FileReader fileReader = new FileReader(file);
+			int charachter;
+			while((charachter = fileReader.read()) != -1){
+				result.add((char) charachter);
+			}
+			fileReader.close();
+		}
+
+		return result;
+	}
+
+	public static void writeToFile(String fileName,String text) throws IOException{
+
+		File file = new File(fileName);
+		FileWriter fileWriter = new FileWriter(file, true);
+		fileWriter.write(text);
+
+		fileWriter.close();
+
+	}
+
+	public static boolean deleteFile(String fileName) throws FileNotFoundException {
+
+		File file = new File(fileName);
+		if (file.exists()) {
+			return file.delete();
+		}
+		else{
+			throw new FileNotFoundException("File wasnt found");
+		}
+
+	}
+
+
+	public static void printArrayOfChars(List<Character> characters){
+		String bettwenResult = "";
+		for(char ch : characters){
+			bettwenResult += ch;
+		}
+
+		System.out.println(bettwenResult);
 	}
 }
